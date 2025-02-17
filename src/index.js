031ccd30fad0bff48b861460a434fd217e2db564
Vikashhh18 <Vikash.sharma1761@gmail.com>
2025-02-17 23:47:06 +0530

    set sequelize server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const {PORT}=require('./config/server');
+const bodyParser=require('body-parser');
 
 const setUpAndStartServer=async ()=>{
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log('server will start at ',PORT);
     })
