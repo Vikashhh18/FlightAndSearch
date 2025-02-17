00d2178fe11c814539c4b397580cd3e00930fd06
Vikashhh18 <Vikash.sharma1761@gmail.com>
2025-02-17 23:00:21 +0530

    setup env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express=require('express');
-require('dotenv').config();
+const {PORT}=require('./config/server');
 
 const setUpAndStartServer=async ()=>{
     const app=express();
-    const port=process.env.PORT;
-    app.listen(port,()=>{
-        console.log('server will start at ',port);
+    app.listen(PORT,()=>{
+        console.log('server will start at ',PORT);
     })
 }
 setUpAndStartServer(); 
